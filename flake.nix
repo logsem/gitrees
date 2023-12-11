f2ce3da14dc5054163bf6baae1b6a633f3db5add
Kaptch <kaptch@gmail.com>
2023-12-11 16:02:39 +0100

    upstream version of iris and stdpp

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "gitrees";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
@@ -11,28 +11,6 @@
         lib = pkgs.lib;
         coq = pkgs.coq_8_17;
         coqPkgs = pkgs.coqPackages_8_17;
-        ocamlPkgs = coq.ocamlPackages;
-        stdpp-dev = coqPkgs.lib.overrideCoqDerivation
-          {
-            defaultVersion = "1.9.0";
-            release."1.9.0".sha256 = "sha256-OXeB+XhdyzWMp5Karsz8obp0rTeMKrtG7fu/tmc9aeI=";
-          } coqPkgs.stdpp;
-        iris-dev = coqPkgs.mkCoqDerivation rec {
-          pname = "iris";
-          domain = "gitlab.mpi-sws.org";
-          owner = "iris";
-          defaultVersion = "4.1.0";
-          release."4.1.0".sha256 = "sha256-nTZUeZOXiH7HsfGbMKDE7vGrNVCkbMaWxdMWUcTUNlo=";
-          releaseRev = v: "iris-${v}";
-
-          propagatedBuildInputs = [ stdpp-dev ];
-
-          preBuild = ''
-            if [[ -f coq-lint.sh ]]
-            then patchShebangs coq-lint.sh
-            fi
-          '';
-        };
       in {
         packages = {
           coq-artifact = coqPkgs.mkCoqDerivation {
@@ -41,8 +19,8 @@
             src = ./.;
             buildPhase = "make";
             propagatedBuildInputs = [
-              stdpp-dev
-              iris-dev
+              coqPkgs.stdpp
+              coqPkgs.iris
               coqPkgs.equations
             ];
           };
@@ -50,10 +28,8 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             coq
-            stdpp-dev
-            iris-dev
-            coqPkgs.equations
           ];
+          inputsFrom = [ self.packages.${system}.coq-artifact ];
         };
       });
 }
